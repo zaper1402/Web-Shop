41825227a0768435076e812654df5fe8a8e2b7fe
zaper1402 <ashirkul@gmail.com>
2024-11-25 19:49:57 +0200

    requirements update

--- backend/webshop_backend/settings.py
+++ backend/webshop_backend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'debug_toolbar',
     'corsheaders'
 ]
 
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'corsheaders.middleware.CorsMiddleware'
 ]
 
--- backend/webshop_backend/urls.py
+++ backend/webshop_backend/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
-] + debug_toolbar_urls()
+]
